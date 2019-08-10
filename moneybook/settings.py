1c9e2739f029249d9f3aac3ae796ea6bbc32acf1
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-10 16:24:18 +0300

    add control check for local settings of project

--- moneybook/settings.py
+++ moneybook/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from moneybook.settings_local import *
+except ImportError:
+    pass
