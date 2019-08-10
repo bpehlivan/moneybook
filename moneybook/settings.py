8cefdda609c151d064b11d7a69e9bf78967ba654
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-10 18:05:19 +0300

    make postgresql database server

--- moneybook/settings.py
+++ moneybook/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.deptors',
+    'apps.invoices',
+    'apps.users'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,11 @@ WSGI_APPLICATION = 'moneybook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moneybook',
+        'USER': 'postgres'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
