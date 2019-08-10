fd5c62b682c1941d6bc1c99f9385cbc9d35bfc04
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-10 17:25:21 +0300

    implement deptor model

--- apps/deptors/models.py
+++ apps/deptors/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+user_model = get_user_model()
+
+
+class Deptor(models.Model):
+    """
+    According to online information(google), maximum length
+    of an IBAN is 34 characters
+    """
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    iban = models.CharField(max_length=34)
+    responsible_admin = models.ForeignKey(user_model,
+                                          on_delete=models.DO_NOTHING)
