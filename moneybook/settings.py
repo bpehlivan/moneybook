78744c687400fad421bcb9283a0682a6dd1ca24e
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-12 01:41:33 +0300

    add social auth pipeline to create user object

--- moneybook/settings.py
+++ moneybook/settings.py
@@ -146,6 +146,18 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '<client_id>'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '<client_secret>'
 
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+
 try:
     from moneybook.settings_local import *
 except ImportError:
