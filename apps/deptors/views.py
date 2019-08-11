656c8f5533971676a5a2561f15f254fd7cbaf16c
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-12 01:49:50 +0300

    add deptor views and templates

--- apps/deptors/views.py
+++ apps/deptors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from apps.deptors.models import Deptor
+
+
+class DeptorView(LoginRequiredMixin, View):
+    def get(self, request):
+        deptors__qs = Deptor.objects.filter(responsible_admin=request.user)
+        return render(request, 'deptors/deptors_list.html',
+                      {'deptors': deptors__qs})
+
+    def post(self, request):
+        import ipdb; ipdb.set_trace()
+        return redirect('debtors:deptors-list')
