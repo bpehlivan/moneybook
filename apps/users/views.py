b5b6793dd30eaab3cd5afe89205ca206f1e5d59a
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-11 11:40:12 +0300

    implement simple views for testing login and home pages

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login/login.html')
+
+    def post(self, request):
+        return redirect('home')
+
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/home.html')
