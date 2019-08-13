fbc8209c0b5c7f73675d78e63e60da15ce71c1b7
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-13 11:22:55 +0300

    refactor debtor list page notifications

--- apps/debtors/views.py
+++ apps/debtors/views.py
@@ -12,13 +12,13 @@ logger = logging.getLogger(__name__)
 
 class DebtorView(LoginRequiredMixin, View):
     @staticmethod
-    def render_debtor_page(request, is_debtor_created=False,
-                           is_debtor_creation_failed=False):
+    def render_debtor_page(request, send_notification=False,
+                           is_operation_succeeded=False):
         debtors__qs = Debtor.objects.filter(responsible_admin=request.user)
         template_data = {
             'debtors': debtors__qs,
-            'is_debtor_created': is_debtor_created,
-            'is_debtor_creation_failed': is_debtor_creation_failed
+            'is_operation_succeeded': is_operation_succeeded,
+            'send_notification': send_notification
         }
         return render(request, 'debtors/debtors_list.html', template_data)
 
@@ -42,12 +42,15 @@ class CreateDebtorView(LoginRequiredMixin, View):
                 first_name=first_name, last_name=last_name,
                 e_mail=e_mail, iban=iban, respobsible_admin=request.user)
 
-            return DebtorView.render_debtor_page(request=request,
-                                                 is_debtor_created=True)
+            return DebtorView.render_debtor_page(
+                request=request, send_notification=True,
+                is_operation_succeeded=True)
+
         except Exception as exc:
             logger.exception(exc)
             return DebtorView.render_debtor_page(
-                request=request, is_debtor_creation_failed=True)
+                request=request, send_notification=True,
+                is_operation_succeeded=False)
 
 
 class DebtorDetailView(LoginRequiredMixin, View):
@@ -62,17 +65,26 @@ class DebtorDetailView(LoginRequiredMixin, View):
     def post(self, request, debtor_id):
         try:
             debtor_instance = Debtor.objects.get(pk=debtor_id)
+        except Debtor.DoesNotExist:
+            return redirect('debtors-list')
+
+        try:
             debtor_service = DebtorService()
             first_name = request.POST.get('first_name')
             last_name = request.POST.get('last_name')
             email = request.POST.get('email')
             iban = request.POST.get('iban')
 
-            updated_instance = debtor_service.update_debtor(
+            debtor_service.update_debtor(
                 instance=debtor_instance, first_name=first_name,
                 last_name=last_name, email=email, iban=iban)
 
-            return render(request, 'debtors/debtor_detail.html',
-                          {'debtor': updated_instance})
-        except Debtor.DoesNotExist:
-            return redirect('debtors-list')
+            return DebtorView.render_debtor_page(
+                request=request, send_notification=True,
+                is_operation_succeeded=True)
+
+        except Exception as exc:
+            logger.exception(exc)
+            return DebtorView.render_debtor_page(
+                request=request, send_notification=True,
+                is_operation_succeeded=False)
