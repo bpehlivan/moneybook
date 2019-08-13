b7a1aa5c8bf232338298284cefaebe288d525ba2
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-13 17:52:15 +0300

    add invoice urls file

--- apps/debtors/urls.py
+++ apps/debtors/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from apps.debtors.views import DebtorView, CreateDebtorView,\
     DebtorDetailView, DeleteDebtorView
@@ -6,9 +6,11 @@ from apps.debtors.views import DebtorView, CreateDebtorView,\
 app_name = 'debtors'
 
 urlpatterns = [
-    path('', DebtorView.as_view(), name='debtors-list'),
+    path('/', DebtorView.as_view(), name='debtors-list'),
     path('create/', CreateDebtorView.as_view(), name='debtors-create'),
     path('<int:debtor_id>/', DebtorDetailView.as_view(), name='debtors-detail'),
     path('<int:debtor_id>/delete/', DeleteDebtorView.as_view(),
-         name='debtors-delete')
+         name='debtors-delete'),
+    path('/', include(('apps.invoices.urls', 'apps.invoices'),
+                      namespace='invoices'))
 ]
