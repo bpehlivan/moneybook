c3565b726c2ddaa39d7718fad3da4085fd59b783
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-13 16:10:22 +0300

    implement invoice service

--- apps/invoices/services.py
+++ apps/invoices/services.py
@@ -7,10 +7,19 @@ from apps.invoices.models import Invoice
 class InvoiceService:
     def create_invoice(self, amount: float, status: str,
                        due_date: date, debtor: Debtor) -> Invoice:
-        raise NotImplementedError()
+        return Invoice.objects.create(amount=amount, status=status,
+                                      due_date=due_date, debtor=debtor)
 
     def update_invoice(self, instance: Invoice, **kwargs) -> Invoice:
-        raise NotImplementedError()
+        update_fields = []
+
+        for k, v in kwargs.items():
+            if hasattr(instance, k):
+                setattr(instance, k, v)
+                update_fields.append(k)
+
+        instance.save(update_fields=update_fields)
+        return instance
 
     def delete_invoice(self, instance: Invoice) -> None:
-        raise NotImplementedError()
+        instance.delete()
