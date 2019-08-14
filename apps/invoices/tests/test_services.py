78a66b920d5741bdd0ecc5f05e935f6f1b16baa7
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 17:52:23 +0300

    add invoice service test as not implemented

---
+++ apps/invoices/tests/test_services.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+
+class InvoiceServiceTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_invoice(self):
+        raise NotImplementedError()
+
+    def test_update_invoice(self):
+        raise NotImplementedError()
+
+    def test_delete_invoice(self):
+        raise NotImplementedError()
