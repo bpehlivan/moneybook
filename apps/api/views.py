6aee6674431fe7baba1650ddb6675ca2a4d923b2
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 10:14:26 +0300

    add api token generation page

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+class ApiKeyGeneratorView(LoginRequiredMixin, View):
+    def get(self, request):
+        qs__token = Token.objects.filter(user=request.user)
+        if qs__token.exists():
+            template_data = {'token': qs__token.get()}
+            return render(request, 'api/api_guide.html', template_data)
+
+        return render(request, 'api/api_guide.html', {})
+
+    def post(self, request):
+        token, _ = Token.objects.get_or_create(user=request.user)
+        return render(request, 'api/api_guide.html', {'token': token})
