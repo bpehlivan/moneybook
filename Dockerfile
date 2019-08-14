871832d9b3c5d08f69138b94730c2d83088799bc
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 19:19:15 +0300

    dockerize project

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.7
+
+ENV PYTHONUNBUFFERED 1
+
+RUN mkdir -p /usr/src/fittrack
+WORKDIR /usr/src/fittrack
+COPY ./requirements.txt requirements.txt
+RUN pip install -r requirements.txt && rm requirements.txt
+RUN  apt-get update && apt-get install -y postgresql-client
+ADD . /usr/src/fittrack/
+RUN chmod +x entrypoint.sh
--- moneybook/settings.py
+++ moneybook/settings.py
@@ -89,8 +89,11 @@ WSGI_APPLICATION = 'moneybook.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moneybook',
-        'USER': 'postgres'
+        'NAME': os.environ.get('DATABASE_NAME', 'moneybook'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': '',
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
 }
 
