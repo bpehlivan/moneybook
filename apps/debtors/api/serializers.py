5296c60956627d5d27188150f8451b603742b4ff
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 11:16:59 +0300

    add invoice counts to debtor serializer

--- apps/debtors/api/serializers.py
+++ apps/debtors/api/serializers.py
@@ -1,9 +1,28 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from apps.debtors.models import Debtor
 
 
-class DebtorSerializer(ModelSerializer):
+class DebtorSerializer(serializers.ModelSerializer):
+    open_invoices = serializers.SerializerMethodField()
+    paid_invoices = serializers.SerializerMethodField()
+    overdue_invoices = serializers.SerializerMethodField()
+    cancelled_invoices = serializers.SerializerMethodField()
+
     class Meta:
         model = Debtor
-        fields = ('first_name', 'last_name', 'email', 'iban',)
+        fields = ('first_name', 'last_name', 'email', 'iban',
+                  'open_invoices', 'paid_invoices', 'overdue_invoices',
+                  'cancelled_invoices', )
+
+    def get_open_invoices(self, obj: Debtor):
+        return obj.open_invoice_count
+
+    def get_paid_invoices(self, obj: Debtor):
+        return obj.paid_invoice_count
+
+    def get_overdue_invoices(self, obj: Debtor):
+        return obj.overdue_invoice_count
+
+    def get_cancelled_invoices(self, obj: Debtor):
+        return obj.cancelled_invoice_count
