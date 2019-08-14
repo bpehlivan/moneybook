169a8d316016233b137ebdc88271886fccef8bc8
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 16:07:25 +0300

    add debtor api filter tests

--- apps/debtors/tests/test_api_views.py
+++ apps/debtors/tests/test_api_views.py
@@ -26,7 +26,7 @@ class DebtorViewSetTestCase(TestCase):
         self.client.force_login(user=user)
 
         # debtors
-        self.debtor_1 = mommy.make(Debtor)
+        self.debtor_1 = mommy.make(Debtor, first_name='foo')
         self.debtor_2 = mommy.make(Debtor)
         self.debtor_3 = mommy.make(Debtor)
 
@@ -44,7 +44,7 @@ class DebtorViewSetTestCase(TestCase):
         mommy.make(Invoice, debtor=self.debtor_3, _quantity=2,
                    status=InvoiceStatusChoices.PAID)
 
-    def test_get(self):
+    def test_list(self):
         url = reverse('api:viewsets:debtors-list')
 
         response = self.client.get(url)
@@ -78,3 +78,43 @@ class DebtorViewSetTestCase(TestCase):
         self.assertEqual(debtor_3_dict['paid_invoices'], 2)
         self.assertEqual(debtor_3_dict['overdue_invoices'], 0)
         self.assertEqual(debtor_3_dict['cancelled_invoices'], 0)
+
+    def test_list__filter(self):
+        # test first name
+        url = '{url}?{filter}={value}'.format(
+            url=reverse('api:viewsets:debtors-list'),
+            filter='first_name', value='foo')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_dict = response.json()
+
+        self.assertEqual(len(response_dict['results']), 1)
+
+        self.assertEqual(response_dict['results'][0]['pk'], self.debtor_1.pk)
+
+        # test invoice count
+        url = '{url}?{filter}={value}'.format(
+            url=reverse('api:viewsets:debtors-list'),
+            filter='invoice_count', value='2')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_dict = response.json()
+
+        self.assertEqual(len(response_dict['results']), 1)
+
+        self.assertEqual(response_dict['results'][0]['pk'], self.debtor_3.pk)
+
+        # test invoice status
+        url = '{url}?{filter}={value}'.format(
+            url=reverse('api:viewsets:debtors-list'),
+            filter='invoice_status', value='paid')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_dict = response.json()
+
+        self.assertEqual(len(response_dict['results']), 2)
