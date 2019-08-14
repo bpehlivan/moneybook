fbed8f7e4c0342dc73abe54c041da86c50ef597a
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 11:21:54 +0300

    add debtor service test as not implemented

---
+++ apps/debtors/tests/test_services.py
@@ -0,0 +1,18 @@
+from model_mommy import mommy
+from django.test import TestCase
+
+from apps.debtors.services import DebtorService
+
+
+class DebtorServiceTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_create_debtor(self):
+        raise NotImplementedError()
+
+    def test_update_debtor(self):
+        raise NotImplementedError()
+
+    def test_delete_debtor(self):
+        raise NotImplementedError()
