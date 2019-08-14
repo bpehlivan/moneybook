e1d9202275a9240e6e4340438cfa563249cc6c4c
burkaypehlivan <burkay.pehlivan@akinon.com>
2019-08-14 14:13:36 +0300

    add debtor viewset to routers

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from apps.api.views import ApiKeyGeneratorView
-
+from apps.debtors.api.views import DebtorViewSet
 
 router = routers.SimpleRouter()
+
+router.register('debtors', DebtorViewSet, 'debtors')
 
 
 urlpatterns = [
     path('api-key/', ApiKeyGeneratorView.as_view(), name='api-key'),
+    path('api/v1', include((router.urls, 'apps.api'), namespace='viewsets'))
 ]
